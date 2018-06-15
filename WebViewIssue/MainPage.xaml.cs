8cd7baa42984f85831179222e1ff25d45751f5e6
JEFFREYHAIN8519\jefhai <jeffhaines@me.com>
2018-06-15 10:42:26 -0400

    initial WebViewIssue app

--- WebViewIssue/MainPage.xaml.cs
+++ WebViewIssue/MainPage.xaml.cs
@@ -1,30 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Collections.Generic;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace WebViewIssue
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += AppBackRequested;
+        }
+
+        private void AppBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (!(Window.Current.Content is Frame rootFrame))
+                return;
+
+            // If we can go back and the event has not already been handled, do so.
+            if (!rootFrame.CanGoBack || e.Handled)
+                return;
+
+            e.Handled = true;
+
+            rootFrame.GoBack();
+        }
+
+        private void VideoOne_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WebViewPage), new Dictionary<string, string>
+            {
+                { "site" , "https://www.youtube.com/watch?v=up863eQKGUI" }
+            });
+        }
+
+        private void VideoTwo_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WebViewPage), new Dictionary<string, string>
+            {
+                { "site" , "https://www.youtube.com/watch?v=MAtayYbOL-o" }
+            });
+        }
+
+        private void VideoThree_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WebViewPage), new Dictionary<string, string>
+            {
+                { "site" , "https://www.youtube.com/watch?v=UQ-RTZCOQn0" }
+            });
         }
     }
 }
---
+++ WebViewIssue/WebViewPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace WebViewIssue
+{
+    public sealed partial class WebViewPage : Page
+    {
+        public WebViewPage()
+        {
+            this.InitializeComponent();
+
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += (s, a) =>
+            {
+                // always try to go back within the WebView first, then try the frame!
+                if (WebViewer.CanGoBack)
+                {
+                    WebViewer.GoBack();
+                    a.Handled = true;
+                    return;
+                }
+                else
+                {
+                    WebViewer.NavigateToString("");
+                }
+            };
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var site = (e.Parameter as Dictionary<string, string>)["site"];
+            WebViewer.Navigate(new Uri(site));
+        }
+    }
+}
